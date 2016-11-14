149ae3d39cc2bcb9ab6fad5ee5a88eca97598825
uhanson <uhanson@perlovka.org>
2016-11-15 01:39:51 +0300

    Functor instance

--- src/Expr.hs
+++ src/Expr.hs
@@ -1,18 +1,28 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE TypeFamilies #-}
 module Expr where
   import Core.Types
 
-  data Expr
+  data ExprF a
     = Lit String
     | Var Name
-    | Let Name Expr
-    | Add Expr Expr
-    | Next Expr Expr
+    | Let Name a
+    | Add a a
+    | Next a a
     deriving (Show)
+
+  data Expr a = Expr { unExpr :: ExprF (Expr a)}
+
+  deriving instance Functor ExprF
+  deriving instance Foldable ExprF
+  deriving instance Traversable ExprF
 
   class LiteralC l r where
     lit :: l -> r
@@ -25,16 +35,16 @@ module Expr where
     add :: r -> r -> r
     next :: r -> r -> r
 
-  instance LiteralC String Expr where
-    lit = Lit
+  instance LiteralC String (Expr a) where
+    lit = Expr . Lit
 
-  instance NameC Name Expr where
-    var = Var
-    let_ = Let
+  instance NameC Name (Expr a) where
+    var = Expr . Var
+    let_ n = Expr . Let n
 
-  instance ExprC Expr where
-    add = Add
-    next = Next
+  instance ExprC (Expr a) where
+    add l = Expr . Add l
+    next l = Expr . Next l
 
   instance LiteralC String String where
     lit = show
@@ -54,9 +64,11 @@ module Expr where
     add l r = l ++ " + " ++ r
     next l r = l ++ "; " ++ r
 
-  --exec :: ExprC a => Expr -> a
-  exec (Lit n) = lit n
-  exec (Var n) = var n
-  exec (Let n e) = let_ n (exec e)
-  exec (Add l r) = add (exec l) (exec r)
-  exec (Next l r) = next (exec l) (exec r)
+  exec :: (LiteralC String a, NameC Name a, ExprC a) => Expr a -> a
+  exec = exec' . unExpr
+    where
+      exec' (Lit n) = lit n
+      exec' (Var n) = var n
+      exec' (Let n e) = let_ n (exec e)
+      exec' (Add l r) = add (exec l) (exec r)
+      exec' (Next l r) = next (exec l) (exec r)
--- src/Main.hs
+++ src/Main.hs
@@ -5,72 +5,58 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE LambdaCase #-}
 module Main where
+  import Control.Applicative
+  import Control.Monad
   import Data.Maybe
   import Data.Monoid
   import Core.Types
   import Expr
   import Query
-{-}
-  instance ExprC (Expr -> Bool) where
-    lit c = \case Lit c' -> c == c'; _ -> False
-    var n = \case Var n' -> n == n'; _ -> False
-    let_ n qe = \case Let n' e -> n == n' && qe e; _ -> False
-    add ql qr = \case Add l r -> ql l && qr r; _ -> False
-    next ql qr = \case Next l r -> ql l && qr r; _ -> False
 
-  instance QueryC (Expr -> Bool) where
-    recQ q = \case
-      e@(Let _ e') -> q e || recQ q e'
-      e@(Add l r) -> q e || recQ q l || recQ q r
-      e@(Next l r) -> q e || recQ q l || recQ q r
-      e -> q e
-    anyQ = const True
-    andQ l r e = l e && r e
-    orQ l r e = l e || r e
--}
-  instance QueryC (Expr -> Maybe [Expr]) where
-    recQ q = \case
-      e@(Let _ e') -> q e <> recQ q e'
-      e@(Add l r) -> q e <> recQ q l <> recQ q r
-      e@(Next l r) -> q e <> recQ q l <> recQ q r
-      e -> q e
+  instance QueryC (Expr a -> Maybe (Expr a)) where
+    recQ q e = case unExpr e of
+      Let _ e' -> q e <|> recQ q e'
+      Add l r -> q e <|> recQ q l <|> recQ q r
+      Next l r -> q e <|> recQ q l <|> recQ q r
+      _ -> q e
 
-    anyQ = Just . pure
+    anyQ = Just
 
-    andQ l r e = do
-      rl <- l e
-      rr <- r e
-      return [e]
+    andQ l r e = liftM2 (const . const e) (l e) (r e)
 
-    orQ l r e = l e <> r e
+    orQ l r e = l e <|> r e
 
-  instance LiteralC String (Expr -> Maybe [Expr]) where
+  instance LiteralC String (Expr a -> Maybe (Expr a)) where
     lit c = lit (c ==)
 
-  instance LiteralC (String -> Bool) (Expr -> Maybe [Expr]) where
-    lit f = \case
-      e@(Lit c) | f c -> pure [e]
-      _ -> mempty
+  instance LiteralC (String -> Bool) (Expr a -> Maybe (Expr a)) where
+    lit f e = case unExpr e of
+      Lit c | f c -> pure e
+      _ -> Nothing
 
-  instance NameC String (Expr -> Maybe [Expr]) where
+  instance NameC Name (Maybe (Expr a)) where
+    var n = pure $ Expr (Var n)
+    let_ n e = Expr . Let n <$> e
+
+  instance NameC Name (Expr a -> Maybe (Expr a)) where
     var n = var (n ==)
     let_ n = let_ (n ==)
 
-  instance NameC (Name -> Bool) (Expr -> Maybe [Expr]) where
-    var f = \case
-      e@(Var n) | f n -> pure [e]
-      _ -> mempty
-    let_ f q = \case
-      e@(Let n e') | f n -> q e' >> return [e]
-      _ -> mempty
+  instance NameC (Name -> Bool) (Expr a -> Maybe (Expr a)) where
+    var f e = case unExpr e of
+      Var n | f n -> pure e
+      _ -> Nothing
+    let_ f q e = case unExpr e of
+      Let n e' | f n -> let_ n (q e')
+      _ -> Nothing
 
-  instance ExprC (Expr -> Maybe [Expr]) where
-    add l r = \case
-      e@(Add el er) -> l el >> r er >> return [e]
-      _ -> mempty
-    next l r = \case
-      e@(Next el er) -> l el >> r er >> return [e]
-      _ -> mempty
+  instance ExprC (Expr a -> Maybe (Expr a)) where
+    add l r e = case unExpr e of
+      Add el er -> liftM2 add (l el) (r er)
+      _ -> Nothing
+    next l r e = case unExpr e of
+      Next el er -> liftM2 next (l el) (r er)
+      _ -> Nothing
 
   expr = let_ "a" ((var "c" `add` var "c") `add` (lit "1" `add` var "b"))
     `next`
@@ -79,12 +65,17 @@ module Main where
   anyV = const True :: String -> Bool
 
   match = recQ ((var anyV `orQ` lit anyV) `add` recQ (var "b"))
+  match2 = recQ (let_ "c" anyQ)
 
   main :: IO ()
   main = do
-    let e = expr :: Expr
+    let e = expr :: Expr String
     putStrLn (expr :: String)
     putStrLn (match :: String)
-    case match e :: Maybe [Expr] of
-      Just es -> mapM_ (putStrLn . exec) es
+    case match e :: Maybe (Expr String) of
+      Just e' -> putStrLn (exec e')
+      Nothing -> putStrLn "Nothing found."
+    putStrLn (match2 :: String)
+    case match2 e :: Maybe (Expr String) of
+      Just e' -> putStrLn (exec e')
       Nothing -> putStrLn "Nothing found."
