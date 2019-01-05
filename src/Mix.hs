be583a07b181cb4afa5ac448132ff5eb08f392af
uhanson <uhanson@perlovka.org>
2019-01-06 02:21:26 +0300

    monad matching

--- src/Mix.hs
+++ src/Mix.hs
@@ -7,16 +7,35 @@
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE LambdaCase #-}
 module Main where
   import Prelude hiding (any)
   import Control.Applicative
+  import Control.Monad
+  import Control.Monad.Writer.Strict hiding (any, Any)
   import Data.Functor.Foldable
   import Data.Monoid ((<>))
+  import Text.Show.Deriving
+ 
+  type Name = String
 
-  import Classes
+  class ExprC a where
+    lit :: Name -> a
+    var :: Name -> a
+    let_ :: a -> a -> a
+    add :: a -> a -> a
+    andNext :: a -> a -> a
+
+  class QueryC a where
+    any :: a
+    has :: a -> a
+    peek :: Name -> a -> a
 
   data ExprF a
     = Lit Name
@@ -24,42 +43,11 @@ module Main where
     | Let a a
     | Add a a
     | AndNext a a
-    deriving (Show, Functor, Foldable, Traversable)
+    deriving (Show)
 
-  data QueryF e a
-    = Match e
-    | Has (QueryF e a)
-    | Any
-    deriving (Show, Functor, Foldable, Traversable)
+  $(deriveShow1 ''ExprF)
 
   type Expr = Fix ExprF
-  
-  newtype Query e a = Query { unQuery :: QueryF (e (Query e a)) a }
-
-  type ExprQ a = Query ExprF a
-
-  deriving instance Show (e (Query e a)) => Show (Query e a)
-
-  class Match q e r where
-    match :: q -> e -> r
- 
-  instance (Alternative f, Match a Expr (f Expr)) => Match (ExprF a) Expr (f Expr) where
-    match (Lit n) e@(Fix (Lit n')) | n == n' = pure e 
-    match (Var n) e@(Fix (Var n')) | n == n' = pure e 
-    match (Add l r) e@(Fix (Add l' r')) = (pure . e <>) <$> match l l' <> match r r'
-
-  instance Applicative f => Match (ExprQ a) Expr (f Expr) where
-    match q e = matchQ (unQuery q) e where
-      matchQ Any e = pure e
-      matchQ (Has q') e = matchQ q' e
-      matchQ (Match e') e = match e' e
-
-  ppExpr = cata pp where
-    pp (Lit v) = v
-    pp (Var v) = v
-    pp (Let l r) = l <> " = " <> r
-    pp (Add l r) = l <> " + " <> r 
-    pp (AndNext l r) = l <> "; " <> r
 
   instance ExprC Expr where
     lit = Fix . Lit
@@ -67,6 +55,19 @@ module Main where
     let_ l = Fix . Let l
     add l = Fix . Add l
     andNext l = Fix . AndNext l
+    
+  type ExprQ a = Query ExprF a
+
+  data QueryF e a
+    = Match e
+    | Peek Name (QueryF e a)
+    | Has (QueryF e a)
+    | Any
+    deriving (Show)
+
+  newtype Query e a = Query { unQuery :: QueryF (e (Query e a)) a }
+
+  deriving instance Show (e (Query e a)) => Show (Query e a)
 
   instance ExprC (ExprQ a) where
     lit = Query . Match . Lit
@@ -78,6 +79,38 @@ module Main where
   instance QueryC (ExprQ a) where
     any = Query Any
     has = Query . Has . unQuery
+    peek n = Query . Peek n . unQuery
+
+  data RMode = RFirst | RAll
+
+  deepFirst :: Monad m => (m Bool) -> (m Bool) -> m Bool
+  deepFirst a b = a >>= \r -> if r then return r else b
+
+  deepAll :: Monad m => (m Bool) -> (m Bool) -> m Bool
+  deepAll a b = (||) <$> a <*> b 
+
+  deep s q e@(Fix (Let l' r'))     f = match q e f `s` deep s q l' f `s` deep s q r' f
+  deep s q e@(Fix (Add l' r'))     f = match q e f `s` deep s q l' f `s` deep s q r' f
+  deep s q e@(Fix (AndNext l' r')) f = match q e f `s` deep s q l' f `s` deep s q r' f
+  deep _ q e f                       = match q e f 
+
+  match :: MonadWriter l m => Query ExprF a -> Expr -> (Expr -> l) -> m Bool
+  match (Query (Match (Lit l))) (Fix (Lit l')) _ | l == l' = pure True 
+  match (Query (Match (Var n))) (Fix (Var n')) _ | n == n' = pure True
+
+  match (Query (Match (Let v e)))     (Fix (Let v' e'))     f = (&&) <$> match v v' f <*> match e e' f
+  match (Query (Match (Add l r)))     (Fix (Add l' r'))     f = (&&) <$> match l l' f <*> match r r' f
+  match (Query (Match (AndNext l r))) (Fix (AndNext l' r')) f = (&&) <$> match l l' f <*> match r r' f
+
+  match (Query (Peek n q)) e f = match (Query q) e f >>= \r -> when r (tell (f e)) >> return r
+
+  match (Query (Has q)) e f = deep deepAll (Query q) e f
+
+  match (Query Any) a _ = return True
+  match _           _ _ = return False
+
+  runMatch :: Query ExprF a -> Expr -> (Bool, [Expr])
+  runMatch q e = runWriter (match q e pure)
 
   e1 :: Expr
   e1 = let_ (var "a") (lit "1")
@@ -86,13 +119,14 @@ module Main where
     `andNext`
       let_ (var "c") (add (var "a") (var "b"))
 
-  qa = add any any
-  qb = has (var "a")
-  qc = any
+  qa = has (peek "a" (let_ any any))
+  
+  qb = has (peek "b" (var "b"))
+  
+  qc = has any
 
   q1 = let_ qc (add qa qb)
 
   main :: IO ()
   main = do
-    putStrLn (ppExpr e1)
-    print (q1 :: ExprQ String)
+    print $ runMatch qa e1
